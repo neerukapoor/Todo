9c00c864e95422641a5dc2ea25cfab73801f4994
Neeru Kapoor <467neeru@gmail.com>
2023-07-08 17:44:23 +0530

    Added post route to add todo

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const path =require('path');
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 4600;
 
 //Middleware  
 app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", require("./routes/indexRoute.ejs"));
+// let todos = ["hello", "yoi"];
+// app.get('/', (req, res) => {
+//     res.render('index', {todos})
+// })
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, ()=>{
